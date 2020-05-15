ed405a7beaceda4cef54cfe240c227d0b1cec0e7
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-05-14 20:19:05 -0500

    changed images to cards

--- src/App/App.js
+++ src/App/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.scss';
 
 import mushroomData from '../helpers/data/mushroomData';
-
 import Basket from '../components/Basket/Basket';
 import Forest from '../components/Forest/Forest';
+
 
 class App extends React.Component {
 state = {
@@ -18,16 +18,29 @@ componentDidMount() {
   this.setState({ mushrooms, basket });
 }
 
+pickAMushroom = (e) => {
+  e.preventDefault();
+  mushroomData.pickAMushroom();
+  const basket = mushroomData.getBasket();
+  this.setState({ basket });
+}
+
 render() {
   return (
       <div className="App">
         <h1>Macabre Mushroom Marsh</h1>
-        <button className="btn btn-dark">Pick A Mushroom</button>
-        <div>
-          <Basket basket={this.state.basket} />
-        </div>
-        <div className='forest'>
-          <Forest mushrooms={this.state.mushrooms} />
+        <button className="btn btn-dark" onClick={this.pickAMushroom}>Pick A Mushroom</button>
+        <div className="container-fluid mt-3">
+          <div className="row">
+            <div className="col-6">
+            <h2 className="divTitleForest">Forest</h2>
+            <Forest mushrooms={this.state.mushrooms} />
+            </div>
+            <div className="col-6">
+            <h2 className="divTitleBasket">Basket</h2>
+            <Basket mushrooms={this.state.basket} />
+            </div>
+          </div>
         </div>
       </div>
   );
